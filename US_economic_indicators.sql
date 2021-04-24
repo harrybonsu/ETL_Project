17cc9ee23a77417b477b8dc086b6ae88ede2a25b
Harry Bonsu <75817600+harrybonsu@users.noreply.github.com>
2021-04-24 00:47:36 -0400

    adding files

---
+++ US_economic_indicators.sql
@@ -0,0 +1,11 @@
+--DROP TABLE US_econ;
+
+CREATE TABLE US_econ(
+		year_ Int,
+		dollar_value Varchar(10),
+		inflation_rate Varchar(10),
+		gdp_rate Varchar(10),
+		m3_rate Varchar(10)		
+);
+
+SELECT * FROM US_econ;
